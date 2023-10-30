36d971737b3827ceaaeb28b8c50ac0f8dbc41bf9
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-10-30 08:55:57 -0300

    🚀 feat: Criando Fetch API wrapper

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-h-home grid grid-cols-9 grid-rows-6 gap-6">
+    <div className="grid max-h-home grid-cols-9 grid-rows-6 gap-6">
       <Link
         href="/"
         className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
@@ -16,7 +16,7 @@ export default function Home() {
           quality={100}
           alt=""
         />
-        <div className="max-w-product-info absolute bottom-28 right-28 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-product-info items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="truncate text-sm">Moletom AI Side</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             R$129
@@ -35,7 +35,7 @@ export default function Home() {
           quality={100}
           alt=""
         />
-        <div className="max-w-product-info absolute bottom-10 right-10 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-product-info items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="truncate text-sm">Moletom Java</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             R$129
@@ -54,7 +54,7 @@ export default function Home() {
           quality={100}
           alt=""
         />
-        <div className="max-w-product-info absolute bottom-10 right-10 flex h-12 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-product-info items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="truncate text-sm">Camiseta DoWhile 2022</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             R$129
